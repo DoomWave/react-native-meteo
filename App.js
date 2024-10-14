a63295d36c841232109265dbc359da0ada4dad32
DoomWave <acunanajair9@gmail.com>
2024-10-14 14:55:41 -0700

    preparing home.

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { s } from "./App.style"
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context"
+import { Home } from "./pages/Home/Home"
 export default function App() {
   return (
   <SafeAreaProvider>
-     <SafeAreaView>
-      
+     <SafeAreaView style={s.container}>
+      <Home/>
      </SafeAreaView>
   </SafeAreaProvider>
   )
---
+++ pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export function Home(){
+    return <>
+    <View>
+        <Text>Basic Weather info</Text>
+    </View>
+    <View>
+        <Text>SearchBar</Text>
+    </View>
+    <View>
+        <Text>Advance Weather info</Text>
+    </View>
+    </>
+}
