915c176eadf34ca3df683b7c8daa18a4a2cc2974
DoomWave <acunanajair9@gmail.com>
2024-10-14 15:15:34 -0700

    image background added.

--- pages/Home/Home.jsx
+++ pages/Home/Home.jsx
@@ -1,15 +1,15 @@
 import { Text, View } from "react-native";
-
+import  { s } from "./Home.style"
 export function Home(){
     return <>
-    <View>
-        <Text>Basic Weather info</Text>
+    <View style={s.meteo_basic}>
+        <Text style={s.txt}>Basic Weather info</Text>
     </View>
-    <View>
-        <Text>SearchBar</Text>
+    <View style={s.meteo_searchbar_container}>
+        <Text style={s.txt}>SearchBar</Text>
     </View>
-    <View>
-        <Text>Advance Weather info</Text>
+    <View style={s.meteo_advanced}>
+        <Text style={s.txt}>Advance Weather info</Text>
     </View>
     </>
 }
