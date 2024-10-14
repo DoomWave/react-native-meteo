8893675f7ef7533e33786fcbe7da50c543fe3332
DoomWave <acunanajair9@gmail.com>
2024-10-14 16:28:18 -0700

    the app ask for permission of location.

--- App.js
+++ App.js
@@ -3,12 +3,35 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context"
 import { Home } from "./pages/Home/Home"
 import { ImageBackground } from "react-native"
 import backgroundImg from "./assets/background.png"
+import { useEffect, useState } from "react"
+import { 
+  requestBackgroundPermissionsAsync, 
+  getCurrentPositionAsync, 
+  requestForegroundPermissionsAsync
+} from "expo-location"
 export default function App() {
+  const [ coordinates, setCoordinates] = useState();
+  useEffect(()=>{
+    getUserCoordinates();
+  }, [])
+
+  async function getUserCoordinates(){
+    const {status} = await requestForegroundPermissionsAsync()
+    if(status==="granted"){
+      const location = await getCurrentPositionAsync()
+      setCoordinates({
+        lat : location.coords.latitude, 
+        lng: location.coords.longitude
+      })
+    }else{
+      setCoordinates({lat: "48.85", lng: "2.35"})
+    }
+  }
   return (
     <ImageBackground imageStyle={s.img} style={s.img_background} source={backgroundImg}>
       <SafeAreaProvider>
         <SafeAreaView style={s.container}>
-          <Home/>
+          <Home />
         </SafeAreaView>
       </SafeAreaProvider>
     </ImageBackground>
