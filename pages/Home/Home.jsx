a561f9034bdb977a057c53edbd41ab7c93c30710
DoomWave <acunanajair9@gmail.com>
2024-10-15 14:11:52 -0700

    getting a weather interpretation and some assets added.

--- pages/Home/Home.jsx
+++ pages/Home/Home.jsx
@@ -2,9 +2,12 @@ import { Text, View } from "react-native";
 import  { s } from "./Home.style"
 import { Txt } from "../../components/Txt/Txt";
 import { MeteoBasic } from "../../components/MeteoBasic/MeteoBasic";
+import { getWeatherInterpretation} from "../../utils/meteo-utils"
 export function Home({weather}){
-    const currentWeather = weather.current_weather
-    return <>
+    const currentWeather = weather.current_weather;
+    const currentInterpretation = getWeatherInterpretation()
+    return (
+    <>
     <View style={s.meteo_basic}>
         <MeteoBasic temperature={Math.round(currentWeather.temperature)}/>
     </View>
@@ -15,4 +18,5 @@ export function Home({weather}){
         <Txt style={s.txt}>Advance Weather info</Txt>
     </View>
     </>
+    )
 }
