40b04f06f7f2ae2c66dd944ff552e280ef148315
DoomWave <acunanajair9@gmail.com>
2024-10-27 16:06:53 -0700

    backbutton added.

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,5 +1,19 @@
+import { TouchableOpacity, View } from "react-native"
 import { s } from "./Header.style.js"
+import { Txt } from "../Txt/Txt.jsx"
+import { useNavigation } from "@react-navigation/native";
 
 export function Header({}) {
-    return <>Header</>
+    const nav = useNavigation()
+    return (
+    <View>
+        <TouchableOpacity onPress={()=> nav.goBack}>
+            <Txt>{"<"}</Txt>
+        </TouchableOpacity>
+        <View>
+           <Txt></Txt>
+           <Txt></Txt>
+        </View>
+    </View>
+    );
 }
