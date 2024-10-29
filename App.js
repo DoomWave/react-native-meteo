64d5f1b889280cfbbe6364a1ef22c687b7e284dc
DoomWave <acunanajair9@gmail.com>
2024-10-29 14:20:59 -0700

    notifications almost work, but just for a real device, i'll continue but without a real device.

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import {
 } from "react-native-safe-area-context"
 import { Home } from "./pages/Home/Home"
 import { Forecasts } from "./pages/Forecasts/Forecasts"
-import { Alert, ImageBackground } from "react-native"
+import { Alert, ImageBackground, Platform } from "react-native"
 import backgroundImg from "./assets/background.png"
 import { useEffect, useState } from "react"
 import { 
@@ -17,6 +17,8 @@ import { MeteoAPI } from "./api/meteo"
 import {useFonts } from "expo-font";
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import * as Notifications from "expo-notifications";
+import * as Device from "expo-device"
 const Stack = createNativeStackNavigator();
 
 const navTheme = {
@@ -34,6 +36,7 @@ export default function App() {
   })
 
   useEffect(()=>{
+    subscribeToNotifications();
     getUserCoordinates();
   }, [])
 
@@ -44,6 +47,36 @@ export default function App() {
     }
   }, [coordinates])
 
+  async function subscribeToNotifications() {
+    let token;
+    if(Platform.OS === 'android'){
+      await Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C'
+      });
+    }
+    if (Device.isDevice) {
+      const { status: existingStatus } = 
+       await Notifications.getPermissionAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status; 
+      }
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token for push notification!");
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+    } else {
+      alert("Must use physical device for Push Notifications")
+    }
+
+    return token;
+  }
   async function fetchWeatherByCoords(coords){
     const WeatherResponse = await MeteoAPI.fetchWeatherByCoords(coords)
     setWeather(WeatherResponse)
